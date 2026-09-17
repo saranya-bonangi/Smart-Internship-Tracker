09009ffbb40d23d95d440be4d42b3c2f298f9036
Saranya Sai Bonangi <saranyasaibonangi@gmail.com>
2026-09-17 17:04:45 +0530

    Added update status feature

--- app.py
+++ app.py
@@ -12,14 +12,15 @@ st.title("💼 Smart Internship Tracker Pro")
 st.markdown("Track your internship applications easily.")
 
 st.sidebar.title("Navigation")
-
 st.sidebar.success("Project 2 - Streamlit Version")
 
 st.header("Dashboard")
 
+# Database Connection
 conn = sqlite3.connect("internships.db")
 cursor = conn.cursor()
 
+# Add Internship Section
 st.subheader("Add Internship")
 
 company = st.text_input("Company Name")
@@ -45,7 +46,7 @@ if st.button("Add Application"):
 
     st.success("Application Added Successfully!")
 
-
+# View Applications
 st.subheader("All Applications")
 
 cursor.execute("SELECT * FROM applications")
@@ -53,6 +54,8 @@ cursor.execute("SELECT * FROM applications")
 applications = cursor.fetchall()
 
 st.table(applications)
+
+# Statistics Section
 st.subheader("Statistics")
 
 cursor.execute("SELECT COUNT(*) FROM applications")
@@ -85,6 +88,8 @@ col2.metric("Applied", applied)
 col3.metric("Interview", interview)
 col4.metric("Rejected", rejected)
 col5.metric("Selected", selected)
+
+# Search Applications
 st.subheader("Search Applications")
 
 search_company = st.text_input("Enter Company Name to Search")
@@ -102,3 +107,34 @@ if search_company:
         st.table(results)
     else:
         st.warning("No applications found.")
+
+# Update Status
+st.subheader("Update Status")
+
+application_id = st.number_input(
+    "Application ID",
+    min_value=1,
+    step=1
+)
+
+new_status = st.selectbox(
+    "New Status",
+    ["Applied", "Interview", "Rejected", "Selected"]
+)
+
+if st.button("Update Status"):
+
+    cursor.execute(
+        """
+        UPDATE applications
+        SET status = ?
+        WHERE id = ?
+        """,
+        (new_status.lower(), int(application_id))
+    )
+
+    conn.commit()
+
+    st.success("Status Updated Successfully!")
+
+    st.rerun()
